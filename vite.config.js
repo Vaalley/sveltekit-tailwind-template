996fdab4dab59dec85a0048901626854793c7c40
Vaalley <vaaleyou@gmail.com>
2023-02-26 19:42:21 +0100

    Added vite serving list template

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 
 const config = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		fs: {
+			// Add specific folders that are outside of the serving list
+			allow: ['..'],
+		},
+	},
 };
 
 export default config;
